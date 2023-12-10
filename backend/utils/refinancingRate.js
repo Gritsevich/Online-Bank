2c359bbf1ea55e602e1533e2aba601307eac1bce
Gritsevich <gritsevich.rina@gmail.com>
2023-12-10 16:37:13 +0300

    запрос для получения ставки

--- backend/utils/refinancingRate.js
+++ backend/utils/refinancingRate.js
@@ -1,14 +1,22 @@
+const { response } = require('express');
 var { doRequest } = require('./httpsRequest');
 
 
 const refinancingOnData = async (date) => {
 
-  response = await doRequest(process.env.RBNB_BANK + 'RefinancingRate?onDate=' + date.toUTCString(),
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // Т.к. JS считает месяцы с 0 до 11, нужно добавить 1
+  const day = date.getDate();
+  const formattedDate = `${year}-${month}-${day}`;
+
+  
+  let responseData = await doRequest(process.env.RBNB_BANK + 'RefinancingRate?onDate=' + formattedDate,
             'GET'
-        );
+  );
  
-  return JSON.parse(response);
+  return JSON.parse(responseData)[0]["Value"];
 }
+
 
 module.exports = {
     refinancingOnData
